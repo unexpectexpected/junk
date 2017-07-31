ee744dda35af33493fc50945dc379b72bcc85121
UnexpectExpected <unexpectexpected@gmail.com>
2017-07-31 14:07:23 +0200

    test changes

--- learning/public/hello.js
+++ learning/public/hello.js
@@ -1,12 +1,17 @@
 var app = angular.module('myApp', []);
 
 app.controller('Hello', function Hello($scope, $http) {
-	$http.get('greeting').then(function successCallback(data) { $scope.greeting = data;});
-	
+	$http.get('greeting').then(function successCallback(response) {
+		$scope.greeting = response.data;
+	});
+
 	$scope.update = function() {
-		$http.get('greeting', {params : {name: $scope.name }}).then(function successCallback(data) {
-			$scope.greeting = data;
+		$http.get('greeting', {
+			params : {
+				name : $scope.name
+			}
+		}).then(function successCallback(response) {
+			$scope.greeting = response.data;
 		});
 	}
-}
-)
+})
